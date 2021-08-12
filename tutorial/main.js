2ffceb8e65493982b5553df4ea8dea6b60f7ab1b
saayanqa2021 <saayan@reckonsys.com>
2021-08-12 21:20:17 +0530

    tutorial files added

---
+++ tutorial/main.js
@@ -0,0 +1,17 @@
+// sinle line comment
+/* multiline comment */
+function msg(){
+         alert("Hello Javatpoint");
+        }
+
+var a = 10;
+var b = 13;
+var a = a+b; // addition
+
+document.write(a);
+//console.log(a);
+
+
+
+
+
