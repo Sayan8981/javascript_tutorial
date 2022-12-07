03bc6cef180d4d992ab607fcbea72ecc7b9ec927
Saayan8981 <saayan8981@gmail.com>
2022-12-07 19:50:31 +0530

    update

---
+++ jquery_tutorial/jquery/javascript.js
@@ -0,0 +1,9 @@
+function function1(){
+    $("#img1").fadeToggle(5000);
+};
+
+function function2(){
+    //$("div").css('background-color', 'red');
+    $(".para1").css('font-style', 'italic');
+    $("#div1,#div3").css('background-color', 'green');
+};
